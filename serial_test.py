a37850f967f6d548257d65ed24236f6bcfc97b70
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2022-03-06 22:02:15 -0500

    udate

--- serial_test.py
+++ serial_test.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         if ser.in_waiting > 0:
             xangle = ser.readline().decode("utf-8").rstrip()
             print("X Angle = {xangle}")
-            pwm = xangle * 5
+            pwm = xangle
             print("X PWM = {pwm}")
             ser.write(b""+pwm+"\n")
     """
