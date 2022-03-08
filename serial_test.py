586e7cd6456337f04958ba10593fbae4f2185b2e
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2022-03-07 19:31:28 -0500

    add simple pid function

--- serial_test.py
+++ serial_test.py
@@ -4,39 +4,40 @@
 import serial 
 import time
 import struct
+from simple_pid import PID
+
+#Define PID Constands
+p = 1
+i = 0
+d = 0
+set_angle = 0
+
+def pid_calc(angle, set):
+    pid = PID(p, i, d, setpoint=set)
+    pid.output_limits(-400, 400)
+    pid_out = pid(angle)
+    if pid > 0:
+        dir = 0
+    elif pid < 0:
+        dir = 1
+    return pid_out, dir        
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 500000, timeout=1)
     ser.reset_input_buffer()
-    
-    
-    #Basic reading serial data from arduino and printing it
 
     while True:
         if ser.in_waiting > 0:
             xangle = ser.readline().decode("utf-8").rstrip()
             print(f"X Angle = {xangle}")
-            pwm = int(float(xangle))
-            
-
-            test_line = ser.readline().decode("utf-8").rstrip()
-            if test_line != xangle:
-                print(f"Recived PWM = {test_line}")
-            else:
-                pid_out = pwm*2
-                print(f"X PWM = {pid_out}")
-                writevar = struct.pack('i', pid_out)
-                ser.write(writevar)
+            xangle = float(xangle)
+            pid_out = pid_calc(xangle, set_angle)
+            print(f"PWM = {pid_out[0]}")
+            print(f"DIR = {pid_out[1]}")
 
 
-    """
-    # Sending arduino data and then having the arduino send back same data
-    # and printing the data from arduino that was originally sent from the pi
-    while True:
-        ser.write(b"Turn motor left!\n")
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
-    """
+            writevar = struct.pack('i', pid_out[0], pid_out[1])
+            ser.write(writevar)
 
-            
+
+
